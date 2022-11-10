57039d8d081ea4d8130e26966297d8acefcc9f6a
Mateus Mattielo Nickhorn <mateus-mn@outlook.com.br>
2022-11-10 17:16:29 -0300

    first commit

---
+++ src/main/java/com/mcj/api/McjApiApplication.java
@@ -0,0 +1,13 @@
+package com.mcj.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McjApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McjApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcj/api/McjApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcj.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McjApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
